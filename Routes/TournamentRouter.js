de0c3e8339d2a8d4200f440918b405cd36b699d9
sanjog-khatri <khatrisanjog6@gmail.com>
2024-09-05 18:58:08 +0545

    tournaments confirmed to be working also necessary changes were made for now

--- Routes/TournamentRouter.js
+++ Routes/TournamentRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { getTournaments, createTournament, updateTournament, deleteTournament } = require('../Controllers/TournamentController');
+const { getTournaments, createTournament, updateTournament, deleteTournament, getTournamentById } = require('../Controllers/TournamentController');
 const authenticateToken = require('../Middlewares/AuthToken');
+const authorizeRoles = require('../Middlewares/AuthRole');
 
-router.get('/', authenticateToken, getTournaments);
-router.post('/create',authenticateToken, createTournament);
-router.put('/:_id',authenticateToken, updateTournament);
-router.delete('/:_id',authenticateToken, deleteTournament);
+router.get('/', authenticateToken, authorizeRoles(['player']), getTournaments);
+router.get('/:_id', authenticateToken, authorizeRoles(['player']), getTournamentById);
+router.post('/create',authenticateToken, authorizeRoles(['owner']), createTournament);
+router.put('/:_id',authenticateToken, authorizeRoles(['owner']), updateTournament);
+router.delete('/:_id',authenticateToken, authorizeRoles(['owner']), deleteTournament);
 
 module.exports = router;
